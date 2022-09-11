4a2ecdd7bfcfd3eb5d18b9f30274fe1224039663
ProfeEgel <45474206+ProfeEgel@users.noreply.github.com>
2022-09-10 19:08:31 -0500

    DentalCare project added

---
+++ DentalCare/DentalCare/Agenda.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Util;
+
+namespace DentalCare
+{
+    internal class Agenda
+    {
+        private List<Day> days;
+        private List<Time> times;
+        private List<Schedule> schedules;
+        private List<Patient> patients;
+        private List<Appointment> appointments;
+        
+        public Agenda()
+        {
+            days = EasyFile<Day>.LoadDataFromFile("days.txt",
+                tokens => new Day(Convert.ToInt32(tokens[0]),
+                                  tokens[1]));
+        }
+    }
+}
---
+++ DentalCare/DentalCare/Patient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DentalCare
+{
+    internal class Patient
+    {
+        public int Id { get; }
+        public string FirstName { get; }
+        public string LastName { get; }
+
+        public string FullName => $"{LastName}, {FirstName}";
+
+        public Patient(int id, string firstName, string lastName)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
