c1398f58abc57ae05cf91b5f505c4b14124b117c
ProfeEgel <45474206+ProfeEgel@users.noreply.github.com>
2022-09-28 11:52:58 -0500

    FormsBasics

---
+++ FormsBasics/FormsBasics/TestForm.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace FormsBasics
+{
+    partial class TestForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTest = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTest = new System.Windows.Forms.TextBox();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTest
+            // 
+            this.btnTest.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTest.Location = new System.Drawing.Point(371, 27);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(84, 32);
+            this.btnTest.TabIndex = 0;
+            this.btnTest.Text = "Test";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(55, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Texto:";
+            // 
+            // txtTest
+            // 
+            this.txtTest.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTest.Location = new System.Drawing.Point(113, 30);
+            this.txtTest.Name = "txtTest";
+            this.txtTest.Size = new System.Drawing.Size(252, 26);
+            this.txtTest.TabIndex = 2;
+            // 
+            // btnShow
+            // 
+            this.btnShow.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnShow.Location = new System.Drawing.Point(371, 65);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(84, 32);
+            this.btnShow.TabIndex = 3;
+            this.btnShow.Text = "Show";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // TestForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 286);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.txtTest);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnTest);
+            this.Name = "TestForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTest;
+        private System.Windows.Forms.Button btnShow;
+    }
+}
+
---
+++ FormsBasics/FormsBasics/TestForm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormsBasics
+{
+    public partial class TestForm : Form
+    {
+        private int contador = 0;
+
+        public TestForm()
+        {
+            InitializeComponent();
+
+            txtTest.Text = "Contador: 0";
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            txtTest.Text = $"Contador: {++contador}";
+            //txtTest.Text = "Esto es una prueba...";
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            switch (MessageBox.Show("Esto es un mensajito...",
+                                    "Aviso",
+                                    MessageBoxButtons.YesNoCancel,
+                                    MessageBoxIcon.Warning))
+            {
+                case DialogResult.Yes:
+                    MessageBox.Show("Selección: SI");
+                    break;
+
+                case DialogResult.No:
+                    MessageBox.Show("Selección: NO");
+                    break;
+
+                case DialogResult.Cancel:
+                    MessageBox.Show("Selección: CANCELAR");
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+}
